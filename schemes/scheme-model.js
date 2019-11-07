332cbb9e49ccc9d7f655acbc177faecff69a2216
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-06 19:16:42 -0500

    Created a dbConfig file and added function frames to the scheme-model file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/dbConfig')
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+
+}
+
+function add(schemes) {
+      
+}
+
+function update(changes, id) {
+      
+}
+
+function remove(id) {
+      
+}
