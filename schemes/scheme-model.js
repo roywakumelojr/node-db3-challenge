1c4ef79e43484a876ab50ebffa80c7a2602b5551
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-06 23:28:39 -0500

    Quaries and Scheme-model complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,21 +14,30 @@ function find() {
 }
 
 function findById(id) {
-    return db('schemes').where({ id }).first();
+    return db('schemes')
+    .where({ id }).first();
 }
 
 function findSteps(id) {
-
+    return db("steps as S")
+    .join("schemes as sc", "sc.id", "=", "S.scheme_id")
+    .where({ scheme_id: id })
+    .select("S.id", "sc.scheme_name", "S.step_number", "S.instructions");
 }
 
 function add(schemes) {
-      
+    return db("schemes")
+    .insert(schemes);          
 }
 
 function update(changes, id) {
-      
+    return db("schemes")
+    .update(changes)
+    .where({ id });      
 }
 
 function remove(id) {
-      
+    return db("schemes")
+    .delete()
+    .where({ id });      
 }
